0931c2a36775fd7d2c3977657d10dcdf6afeaf8a
Asheniac <asheniac@gmail.com>
2019-06-19 22:22:49 +1000

    updated

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,8 +1,15 @@
 import { Controller, Get, Req, Query, Headers, Param, Post, Body } from '@nestjs/common';
 import { CreatePostDto } from './post.dto';
+import { DemoService } from './provider/demo/demo.service';
 
 @Controller('posts')
 export class PostsController {
+    private readonly demoService;
+
+    constructor(demoService:DemoService){
+       this.demoService=demoService;
+    }
+
     @Get()
     index(@Headers() headers){
         console.log(headers)
