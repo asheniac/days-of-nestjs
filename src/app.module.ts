42025d0c50c0454d0082fb1176c7e0d5ff06346d
Asheniac <asheniac@gmail.com>
2019-06-23 20:00:07 +1000

    middlewares

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PostsController } from './posts/posts.controller';
-import { DemoService } from './posts/provider/demo/demo.service';
-import { PostsModule } from './posts/posts.module';
+
+import { PostsModule } from './modules/posts/posts.module';
 
 @Module({
   imports: [PostsModule],
-  controllers: [AppController, PostsController],
-  providers: [AppService, DemoService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
